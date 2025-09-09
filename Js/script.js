d37a3ef216aad5e159c6304970f2b346ed2f9061
amrkhalid99 <amrkhalid99@github.com>
2025-09-09 16:22:28 -0400

    edit lang to AR

--- Js/script.js
+++ Js/script.js
@@ -2,9 +2,9 @@ const mode = document.getElementById("modecolor");
 mode.addEventListener("click", () => {
     document.body.classList.toggle("dark")
     if (document.body.classList.contains("dark")) {
-        mode.textContent = "Light Mode"
+        mode.textContent = "الوضع النهاري"
     } else {
-        mode.textContent = "Dark Mode"
+        mode.textContent = "الوضع الليلي"
     }
 }
 )    
